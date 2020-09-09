23914c12342cf70f0fdb670ac387820f9f6011df
Jakub Kupczyk <jkbkupczyk@gmail.com>
2020-09-09 23:35:31 +0200

    Added mapping for ISO

--- src/main/java/pl/kupczyk/covidclient/api/CovidService.java
+++ src/main/java/pl/kupczyk/covidclient/api/CovidService.java
@@ -5,22 +5,24 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import pl.kupczyk.covidclient.model.CovidData;
 import pl.kupczyk.covidclient.model.Data;
+import pl.kupczyk.covidclient.model.Region;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class CovidService {
 
-    public String url = "https://raw.githubusercontent.com/" +
-            "CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/";
-
-    public String URL = "https://covid-api.com/api/reports";
+    public static final String URL = "https://covid-api.com/api/reports";
 
     public String generateFile(){
+        String url = "https://raw.githubusercontent.com/" +
+                "CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/";
+
         LocalDate date = LocalDate.now().minusDays(1);
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM-dd-yyyy");
         url = url + date.format(formatter) + ".csv";
@@ -32,11 +34,18 @@ public class CovidService {
         RestTemplate restTemplate = new RestTemplate();
         CovidData list = restTemplate.getForObject(URL, CovidData.class);
 
-        return list.getData();
+        return Objects.requireNonNull(list).getData();
     }
 
     public List<Integer> getConfirmed(){
         return downloadData().stream().map(Data::getConfirmed)
+                .collect(Collectors.toList());
+    }
+
+    public List<String> getIso(){
+        List<Region> list = (List<Region>) downloadData().get(0).getRegion();
+
+        return list.stream().map(Region::getIso)
                 .collect(Collectors.toList());
     }
 
